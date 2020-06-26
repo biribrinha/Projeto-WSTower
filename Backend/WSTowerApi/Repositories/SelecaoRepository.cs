64e9b79c1b121a79bf4cd84210f7950ae67fe85d
MattSouuza <matheus_ss02@hotmail.com>
2020-06-25 21:36:12 -0300

    Working on the selecaorepository

--- Backend/WSTowerApi/Repositories/SelecaoRepository.cs
+++ Backend/WSTowerApi/Repositories/SelecaoRepository.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 
 namespace WSTowerApi.Repositories
 {
@@ -14,14 +15,16 @@ namespace WSTowerApi.Repositories
     {
         WSTowerApiContext ctx = new WSTowerApiContext();
 
-        public void Atualizar(int id, Selecao selecaoAtualizada)
+        private string stringConexao = "Data Source=DESKTOP-4EGEC9A\\SQLEXPRESS; Initial Catalog=Campeonato; Integrated Security=true;";
+
+        public void Atualizar(int id, Selecao selecaoNova)
         {
             Selecao selecaoBuscada = ctx.Selecao.Find(id);
 
-            selecaoBuscada.Nome = selecaoAtualizada.Nome;
-            selecaoBuscada.Bandeira = selecaoAtualizada.Bandeira;
-            selecaoBuscada.Uniforme = selecaoAtualizada.Uniforme;
-            selecaoBuscada.Escalacao = selecaoAtualizada.Escalacao;
+            selecaoBuscada.Nome = selecaoNova.Nome;
+            selecaoBuscada.Bandeira = selecaoNova.Bandeira;
+            selecaoBuscada.Uniforme = selecaoNova.Uniforme;
+            selecaoBuscada.Escalacao = selecaoNova.Escalacao;
 
             ctx.Selecao.Update(selecaoBuscada);
 
@@ -37,13 +40,47 @@ namespace WSTowerApi.Repositories
 
         public List<Selecao> ListarJogadoresOrdemAlfabetica(int idSelecao)
         {
-            //Selecao selecaoSelecionada = new Selecao();
-            //foreach (var jogador in selecaoSelecionada.Jogadores)
-            //{
-                
-            //}
+            List<Selecao> selecoes = new List<Selecao>();
 
-            return ctx.Selecao.Include(s => s.Jogador).OrderBy(s => s.Jogador).Where(s => s.Id == idSelecao).ToList();
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                string querySelect = "SELECT S.Nome, S.Bandeira, J.Nome, J.Foto, J.Posicao FROM Selecao S" +
+                                     "INNER JOIN Jogador J" +
+                                     "ON S.Id = J.SelecaoID" +
+                                     "WHERE S.Id = @ID" +
+                                     "ORDER BY J.Nome ASC;";
+
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(querySelect, con))
+                {
+                    cmd.Parameters.AddWithValue("@ID", idSelecao);
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        Selecao selecao = new Selecao
+                        {
+                            Nome = rdr["Nome"].ToString(),
+                            Bandeira = Convert.ToByte(rdr["Banderira"]),
+
+                            Jogador = new Jogador
+                            {
+                                Nome = rdr["Nome"].ToString(),
+                                Foto = rdr["Foto"].ToByte(),
+                                Posicao = rdr["Posicao"],
+                            }
+
+                        };
+
+                        selecoes.Add(selecao);
+                    }
+                }
+            }
+
+            return selecoes;
+            //return ctx.Selecao.Include(s => s.Jogador).OrderBy(s => s.Jogador).Where(s => s.Id == idSelecao).ToList();
         }
 
         public List<Selecao> ListarJogadoresOrdemDecrescente(int idSelecao)
@@ -58,7 +95,7 @@ namespace WSTowerApi.Repositories
 
         public List<Selecao> ListarJogadoresPorSelecao(int idSelecao)
         {
-            return ctx.Selecao.Include(s => s.Jogador).Where(s => s.Id == idSelecao).ToList();
+            return ctx.Selecao.Include(s => s.Jogadores).Where(s => s.Id == idSelecao).ToList();
         }
 
         public Selecao ListarPorId(int id)
